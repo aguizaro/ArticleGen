8a3443e1e52d7f83c794a8244d4c3542ef62eed0
Tony G <aguizaro@ucsc.edu>
2025-07-08 20:36:48 -0700

    append to log file

--- scripts/collect_articles.sh
+++ scripts/collect_articles.sh
@@ -2,4 +2,4 @@
 
 LOG_FILE="/home/bitnami/ArticleGen/logs/collection/collect_articles_$(date +%Y-%m-%d).log"
 
-/usr/bin/docker exec backend node /app/collectArticles.js > "$LOG_FILE" 2>&1
+/usr/bin/docker exec backend node /app/collectArticles.js >> "$LOG_FILE" 2>&1
